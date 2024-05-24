d244381b2a24bcfe7da904360d854d60f2cbfc1b
Nicolas Cortial <75365622+IreshDeragon@users.noreply.github.com>
2024-05-24 10:09:28 +0200

    Shader d'échiquier

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -11,6 +11,19 @@ void main()
     //out_color = colo;
     //out_color = vec4(vertex_position, 1., 1.);
     //out_color = vec4(uv.x, uv.y, 0., 1.);
-    vec4 texture_color = texture(my_texture, uv);
-    out_color = texture_color;
+    //vec4 texture_color = texture(my_texture, uv);
+    //out_color = texture_color;
+    int x = int(uv.x * 8.0);
+    int y = int(uv.y * 8.0);
+    bool isBlack = mod(x + y, 2) == 1;
+    
+    // Définir la couleur en fonction de l'emplacement de l'échiquier
+    if (isBlack)
+    {
+        out_color = vec4(0.0, 0.0, 0.0, 1.0); // Noir
+    }
+    else
+    {
+        out_color = vec4(1.0, 1.0, 1.0, 1.0); // Blanc
+    }
 }
